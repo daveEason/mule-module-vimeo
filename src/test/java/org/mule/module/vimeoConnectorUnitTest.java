c9a87b70ac13b024ff8c44715eb709a4ca19a354
David Eason <david.eason@mulesoft.com>
2012-08-01 12:03:11 -0700

    Continued development - added mule.http.transport to POM file for testing and updated mule config file used in testing.

---
+++ src/test/java/org/mule/module/vimeoConnectorUnitTest.java
@@ -0,0 +1,11 @@
+package org.mule.module;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: davideason
+ * Date: 8/1/12
+ * Time: 10:09 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class vimeoConnectorUnitTest {
+}
